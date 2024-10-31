5ee26df6017f0223f7e611255faeb0659ac0d15d
selim0711 <selimbahar@hotmail.de>
2024-10-31 10:35:10 +0100

    pushing assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Wöchentliche Berichte - Jedes Gruppenmitglied erzählt was er gemacht hat.
+
+
+    // Versionskontrollsytem - Git
+    // GDD und TDD
+    // Spielidee vorstellen
+
+    // TDD
+
+}
